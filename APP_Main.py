2c5042ac699b4d0a96fd8258f9291d838f7181f9
PH59-GD <hammelpayton@gmail.com>
2026-09-07 12:28:45 -0500

    Add files via upload

--- APP_Main.py
+++ APP_Main.py
@@ -1,1 +1,22 @@
+import time
+UD = {"JSmith": 1234}
+LM = 0
+while LM != 1:
+    print("Welcome user!")
+    print("")
+    print("[1] Login")
+    print("[2] Sign up")
+    print("")
+    UC = input("Select a number: ")
+    if(UC == "1"):
+        print("This is the login screen.")
+        print("")
+        print("Enter your username:")
+        UN = input()
+        print("")
+        print("Enter your password:")
+        UP = input()
 
+    elif(UC == "2"):
+        print("This is the sign up screen.")
+time.sleep(60)
