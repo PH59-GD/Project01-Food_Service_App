7087252c1ab89b88bd1a80fb1bf45b1e67c0e419
PH59-GD <hammelpayton@gmail.com>
2026-09-09 09:31:04 -0500

    Add files via upload

--- APP_Main.py
+++ APP_Main.py
@@ -1,22 +1,32 @@
 import time
 UD = {"JSmith": 1234}
-LM = 0
-while LM != 1:
-    print("Welcome user!")
-    print("")
-    print("[1] Login")
-    print("[2] Sign up")
-    print("")
-    UC = input("Select a number: ")
-    if(UC == "1"):
-        print("This is the login screen.")
+def LoginScr():
+    LM = 0
+    while LM != 1:
+        print("Welcome user!")
         print("")
-        print("Enter your username:")
-        UN = input()
+        print("[1] Login")
+        print("[2] Sign up")
         print("")
-        print("Enter your password:")
-        UP = input()
+        UC = input("Select a number: ")
 
-    elif(UC == "2"):
-        print("This is the sign up screen.")
-time.sleep(60)
+        if(UC == "1"):
+            #print(UD)
+            print("This is the login screen.")
+            print("")
+            print("Enter your username:")
+            UN = input()
+            print("")
+            print("Enter your password:")
+            UP = input()
+            if(UN in UD.keys() and int(UP) == UD[UN]):
+                print("Logging in...")
+                print("")
+            else:
+                print("")
+                print("Incorrect Username or Password!")
+
+
+        elif(UC == "2"):
+            print("This is the sign up screen.")
+LoginScr()
