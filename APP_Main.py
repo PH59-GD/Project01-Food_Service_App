cd1ac5a27d74551b867ad6d0b274294860b581ed
PH59-GD <hammelpayton@gmail.com>
2026-09-10 15:14:22 -0500

    Add files via upload

--- APP_Main.py
+++ APP_Main.py
@@ -1,7 +1,29 @@
 import time
-UD = {"JSmith": 1234}
+
+UD = {"JSmith": {"Password": {"1234": "John Smith"}}, }
+CU = ""
+def UsrMenu():
+    global CU
+    UMM = 0
+    while UMM != 1:
+        print('\x1bc')
+        print("Welcome, "+CU)
+        print(" ______________________________________")
+        print("|TABS:    |[1]Account|[2]Stores|[3]Cart|")
+        print("")
+        print("")
+        print("")
+        print("")
+        print("")
+        print(UD)
+        
+        UC = input()
 def LoginScr():
+    global CU
     LM = 0
+    print(UD)
+    print(UD.keys)
+    
     while LM != 1:
         print("Welcome user!")
         print("")
@@ -9,7 +31,7 @@ def LoginScr():
         print("[2] Sign up")
         print("")
         UC = input("Select a number: ")
-
+        print('\x1bc')
         if(UC == "1"):
             #print(UD)
             print("This is the login screen.")
@@ -19,8 +41,13 @@ def LoginScr():
             print("")
             print("Enter your password:")
             UP = input()
-            if(UN in UD.keys() and int(UP) == UD[UN]):
+            
+            if(UN in UD.keys() and UP in UD[UN]["Password"].keys()):
+                print('\x1bc')
+                CU = UD[UN]["Password"][UP]
                 print("Logging in...")
+                time.sleep(0.5)
+                UsrMenu()
                 print("")
             else:
                 print("")
@@ -30,6 +57,7 @@ def LoginScr():
         elif(UC == "2"):
             SM = 0
             while SM != 1:
+                print('\x1bc')
                 print("This is the sign up screen.")
                 print("")
                 print("Welcome New User!")
@@ -38,9 +66,18 @@ def LoginScr():
                 NUN = input()
                 if len(NUN) == 0:
                     print("Error! Username cannot be blank")# or a number!")
+                    time.sleep(1)
                 else:
                     print("Enter your password:")
                     NUP = input()
+                    print("")
+                    print("Enter your name(EX: John Smith):")
+                    NN = input()
+                    UD[NUN] = {"Password": {NUP: NN}}
+                    
+                    LoginScr()
+#print(UD["JSmith"]["Password"].keys())
 
 
 LoginScr()
+#UsrMenu()
